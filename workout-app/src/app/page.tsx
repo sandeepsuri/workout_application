772af47cd18485835aaeccf53ff3619db012f032
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2023-08-09 21:49:58 -0400

    fix: moved css into global css

--- workout-app/src/app/page.tsx
+++ workout-app/src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import GoogleButton from '../components/login/googleSignIn';
@@ -13,18 +14,9 @@ export default function Home() {
   })
 
   return (
-    <div
-      style={{
-        backgroundImage: `url("/bghome.jpg")`, // Adjust the path as needed
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        minHeight: '100vh', // Make sure the background covers the entire viewport height
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust opacity by changing the alpha value (0.5 in this example)
-      }}
-    >
+    <div className='homeWallpaper'>
       <div className="flex flex-col items-center justify-center h-screen">
-        <img
+        <Image
           src="/Logo.svg"
           alt="Greatest Workout App"
           width={2040}
