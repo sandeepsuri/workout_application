06a826e2d49b178825d50556333ddb458cc4e411
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2023-10-11 14:10:53 -0400

    wip: leaderboard minimized

--- workout-app/src/components/weekpot.tsx
+++ workout-app/src/components/weekpot.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 const weekpot = () => {
   return (
     <div>
-        <div className=" bg-red-400">Week/Pot view</div> 
+        <div className="w-80 h-40 justify-center items-center inline-flex">
+          <div className="w-80 h-40 bg-slate-800 rounded-2xl border-l-4 border-r-4 border-t-8 border-b-8 border-cyan-900 flex-col justify-center items-center inline-flex">
+            <div className="w-72 h-20 text-center text-black text-5xl font-medium font-['Reenie Beanie'] leading-loose">$300</div>
+            <div className="px-8 bg-gray-900 justify-center items-center inline-flex">
+              <div className="text-center text-white text-opacity-80 text-sm font-light font-['Roboto'] leading-loose tracking-widest">LEADERBOARD</div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
