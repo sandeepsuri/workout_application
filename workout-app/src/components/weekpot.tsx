38725b9de949a76be6040e3b6c8d5fe07e1ce2a8
Harvir Sidhu <harvir.s.sidhu@gmail.com>
2023-10-11 21:46:05 -0400

    added button functionality

--- workout-app/src/components/weekpot.tsx
+++ workout-app/src/components/weekpot.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const weekpot = () => {
+const weekpot: React.FC = () => {
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setButtonClicked(!buttonClicked);
+  };
+
   return (
-    <div>
-        <div className=" bg-red-400">Week/Pot view</div> 
+    <div className="flex justify-center items-start min-h-96 p-4">
+      <button
+        className={`w-${buttonClicked ? 'full' : '52'} h-[20px] px-4 py-[16px] rounded-full justify-center items-center inline-flex bg-red-700 text-white`}
+        onClick={handleClick}
+        style={{ fontFamily: 'Impact' }}
+      >
+        {buttonClicked ? 'WEEK 16 out of 28' : 'WEEK 16'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default weekpot
+export default weekpot;
