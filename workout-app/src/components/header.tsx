542ad6289ff423b1ea5bca0ff7d07a0c7d58eeed
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2023-10-20 16:29:46 -0400

    wip: leaderboard css updates

--- workout-app/src/components/header.tsx
+++ workout-app/src/components/header.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const header = () => {
   return (
     <div>
-        <div className="w-96 h-20 px-24 py-3 bg-gray-700 shadow justify-center items-center inline-flex">
-        <div className="w-52 h-14 relative flex-col justify-start items-start flex">
-        <div className="w-52 h-14 relative">
-<div className="w-52 h-14 left-0 top-0 absolute" />
-<div className="w-28 h-5 left-[42px] top-[17px] absolute text-center text-white text-sm font-white font-['Inter']">WORKOUT APP</div>
-</div>
-        </div>
+        <div className="max-w-full h-20 px-28 bg-gray-700 shadow justify-center items-center inline-flex">
+          <div className="w-full h-14 relative flex-col justify-start items-start flex">
+            <div className="w-52 h-14 relative">
+              <div className="w-52 h-14 left-0 top-0 absolute" />
+              <div className="w-28 h-5 left-[42px] top-[17px] absolute text-center text-white text-sm font-white font-['Inter']">WORKOUT APP</div>
+            </div>
+          </div>
         </div>
     </div>
   )
--- workout-app/src/components/weekpot.tsx
+++ workout-app/src/components/weekpot.tsx
@@ -20,75 +20,49 @@ const weekpot = ({toggleBoard, handleToggle}) => {
           </div>
         </div>
         :
-        <div className="w-80 h-96 relative" onClick={handleToggle}>
-          <div className="w-80 h-96 px-1 pt-7 pb-96 left-0 top-0 absolute bg-slate-800 rounded-2xl border-l-4 border-r-4 border-t-8 border-b-8 border-cyan-900 flex-col justify-start items-center inline-flex">
+        <div className="w-80 h-[32rem] relative" onClick={handleToggle}>
+          <div className="w-80 h-[32rem] px-1 pt-7 pb-96 left-0 top-0 absolute bg-slate-800 rounded-2xl border-l-4 border-r-4 border-t-8 border-b-8 border-cyan-900 flex-col justify-start items-center inline-flex">
               <div className="w-72 h-20 text-center text-black text-5xl font-medium font-['Reenie Beanie'] leading-loose">$300</div>
           </div>
-          <div className="w-64 h-72 left-[26px] top-[194px] absolute flex-col justify-center items-start gap-5 inline-flex">
-              <div className="w-64 h-7 relative flex-col justify-start items-start flex">
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">$0.00</div>
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">Shanthosh Y</div>
-                  <div className="w-5 h-7 relative">
-                      <div className="w-5 h-5 left-0 top-[4px] absolute bg-black rounded-full shadow border border-yellow-400" />
-                      <div className="w-6 h-6 p-2.5 left-[-1px] top-[3px] absolute flex-col justify-center items-center gap-2.5 inline-flex">
-                          <div className="w-1.5 h-6 text-center text-yellow-400 text-xs font-medium font-['Inter'] leading-loose">1</div>
-                      </div>
-                  </div>
-                  <div className="w-64 h-px border border-sky-600"></div>
+
+          <div className="w-64 h-72 left-[26px] top-[194px] absolute flex-col justify-center items-start gap-5 inline-flex overflow-auto">
+              
+
+            <div className="w-64 flex">
+              <div className="w-5 h-7 relative text-center">
+                <div className="w-5 h-5 bg-black rounded-full shadow border border-yellow-400">
+                  <div className="w-6 left-0 text-center text-yellow-400 text-xs font-medium font-['Inter'] leading-loose mx-auto">1</div>
+                </div>
               </div>
-              <div className="w-64 h-7 relative flex-col justify-start items-start flex">
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">$20.00</div>
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">Gurkirat G</div>
-                  <div className="w-5 h-7 relative">
-                      <div className="w-5 h-5 left-0 top-[4px] absolute bg-black rounded-full shadow border border-stone-300" />
-                      <div className="w-6 h-6 p-2.5 left-[-1px] top-[3px] absolute flex-col justify-center items-center gap-2.5 inline-flex">
-                          <div className="w-1.5 h-6 text-center text-stone-300 text-xs font-medium font-['Inter'] leading-loose">2</div>
-                      </div>
-                  </div>
-                  <div className="w-64 h-px border border-sky-600"></div>
+              <div className="w-32 mr-20 text-white text-xs font-normal font-['Roboto'] leading-loose">Shanthosh Y</div>
+              <div className="w-24 left-[10px] text-white text-xs font-normal font-['Roboto'] leading-loose text-right">$0.00</div>
+            </div>
+            <div className="w-64 h-px border border-sky-600"></div>
+
+            <div className="w-64 flex">
+              <div className="w-5 h-7 relative text-center">
+                <div className="w-5 h-5 bg-black rounded-full shadow border border-yellow-400">
+                  <div className="w-6 left-0 text-center text-yellow-400 text-xs font-medium font-['Inter'] leading-loose mx-auto">1</div>
+                </div>
               </div>
-              <div className="w-64 h-7 relative flex-col justify-start items-start flex">
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">$40.00</div>
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">Harvir S</div>
-                  <div className="w-5 h-7 relative">
-                      <div className="w-5 h-5 left-0 top-[4px] absolute bg-black rounded-full shadow border border-amber-600" />
-                      <div className="w-6 h-6 p-2.5 left-[-1px] top-[3px] absolute flex-col justify-center items-center gap-2.5 inline-flex">
-                          <div className="w-1.5 h-6 text-center text-amber-600 text-xs font-medium font-['Inter'] leading-loose">3</div>
-                      </div>
-                  </div>
-                  <div className="w-64 h-px border border-sky-600"></div>
+              <div className="w-32 mr-20 text-white text-xs font-normal font-['Roboto'] leading-loose">Shanthosh Y</div>
+              <div className="w-24 left-[10px] text-white text-xs font-normal font-['Roboto'] leading-loose text-right">$0.00</div>
+            </div>
+            <div className="w-64 h-px border border-sky-600"></div>
+
+
+            <div className="w-64 flex">
+              <div className="w-5 h-7 relative text-center">
+                <div className="w-5 h-5 bg-black rounded-full shadow border border-yellow-400">
+                  <div className="w-6 left-0 text-center text-yellow-400 text-xs font-medium font-['Inter'] leading-loose mx-auto">1</div>
+                </div>
               </div>
-              <div className="w-64 h-7 relative flex-col justify-start items-start flex">
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">$60.00</div>
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">Jimi P</div>
-                  <div className="w-5 h-7 py-0.5 flex-col justify-center items-center inline-flex">
-                      <div className="self-stretch grow shrink basis-0 p-2.5 flex-col justify-center items-center gap-2.5 inline-flex">
-                          <div className="w-6 h-6 text-center text-white text-xs font-medium font-['Inter'] leading-loose">4</div>
-                      </div>
-                  </div>
-                  <div className="w-64 h-px border border-sky-600"></div>
-              </div>
-              <div className="w-64 h-7 relative flex-col justify-start items-start flex">
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">$80.00</div>
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">Sandeep S</div>
-                  <div className="w-5 h-7 py-0.5 flex-col justify-center items-center inline-flex">
-                      <div className="self-stretch grow shrink basis-0 p-2.5 flex-col justify-center items-center gap-2.5 inline-flex">
-                          <div className="w-6 h-6 text-center text-white text-xs font-medium font-['Inter'] leading-loose">5</div>
-                      </div>
-                  </div>
-                  <div className="w-64 h-px border border-sky-600"></div>
-              </div>
-              <div className="w-64 h-7 relative flex-col justify-start items-start flex">
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">$100.00</div>
-                  <div className="w-16 text-white text-xs font-normal font-['Roboto'] leading-loose">Deshane M</div>
-                  <div className="w-5 h-7 py-0.5 flex-col justify-center items-center inline-flex">
-                      <div className="self-stretch grow shrink basis-0 p-2.5 flex-col justify-center items-center gap-2.5 inline-flex">
-                          <div className="w-6 h-6 text-center text-white text-xs font-medium font-['Inter'] leading-loose">6</div>
-                      </div>
-                  </div>
-                  <div className="w-64 h-px border border-sky-600"></div>
-              </div>
+              <div className="w-32 mr-20 text-white text-xs font-normal font-['Roboto'] leading-loose">Shanthosh Y</div>
+              <div className="w-24 left-[10px] text-white text-xs font-normal font-['Roboto'] leading-loose text-right">$0.00</div>
+            </div>
+            <div className="w-64 h-px border border-sky-600"></div>
           </div>
+          
           <div className="w-56 h-7 left-[41px] top-[149px] absolute">
               <div className="left-[172px] top-0 absolute text-center text-white text-xs font-light font-['Roboto'] leading-loose">Contribution</div>
               <div className="left-0 top-0 absolute text-center text-white text-xs font-light font-['Roboto'] leading-loose">Rank</div>
