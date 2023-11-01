98b25d6da846fc458a858c7b366f3fe7d9dd22f8
Jimi <122390267+jimip85@users.noreply.github.com>
2023-11-01 19:55:34 -0400

    Header component w/ Logo

--- workout-app/src/app/Dashboard/page.tsx
+++ workout-app/src/app/Dashboard/page.tsx
@@ -1,42 +1,43 @@
-'use client'
-import { useRouter } from 'next/navigation';
-import WeekTracker from '@/components/weekTracker';
+"use client";
+import { useRouter } from "next/navigation";
+import WeekTracker from "@/components/weekTracker";
 import Weekpot from "@/components/weekpot";
 import Header from "@/components/header";
 import Log from "@/components/log";
 import { app } from "@/firebase/config";
 import React, { useState, useEffect } from "react";
-import { 
-  browserLocalPersistence, 
-  getRedirectResult, 
-  onAuthStateChanged, 
-  setPersistence 
-} from "firebase/auth"
-
-
+import {
+  browserLocalPersistence,
+  getRedirectResult,
+  onAuthStateChanged,
+  setPersistence,
+} from "firebase/auth";
 
 export default function Dashboard() {
   const router = useRouter();
   // const [isClient, setIsClient] = useState(false);
-  const [toggleBoard, setToggleBoard] = useState(false)
+  const [toggleBoard, setToggleBoard] = useState(false);
 
   const handleToggle = () => {
-    setToggleBoard(!toggleBoard)
-  }
+    setToggleBoard(!toggleBoard);
+  };
 
   const signOut = async () => {
-    router.push('/')
-  }
-  
+    router.push("/");
+  };
 
   return (
-  <div className="flex flex-col items-center h-screen">
-    <Header />
-    <WeekTracker />
-    <Weekpot toggleBoard={toggleBoard} handleToggle={handleToggle} />
-    {toggleBoard ? null : <Log />}
-    {/* <Log /> */}
-    <footer className="bg-black mt-2"><button onClick={signOut} className="mt-4 text-white">Sign Out</button></footer>
-  </div>  
-  )
+    <div className="flex flex-col items-center h-screen bg-gray-800">
+      <Header />
+      <WeekTracker />
+      <Weekpot toggleBoard={toggleBoard} handleToggle={handleToggle} />
+      {toggleBoard ? null : <Log />}
+      {/* <Log /> */}
+      <footer className="bg-black mt-2">
+        <button onClick={signOut} className="mt-4 text-white">
+          Sign Out
+        </button>
+      </footer>
+    </div>
+  );
 }
--- workout-app/src/components/header.tsx
+++ workout-app/src/components/header.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const header = () => {
   return (
     <div>
-        <div className="max-w-full h-20 px-28 bg-gray-700 shadow justify-center items-center inline-flex">
-          <div className="w-full h-14 relative flex-col justify-start items-start flex">
-            <div className="w-52 h-14 relative">
-              <div className="w-52 h-14 left-0 top-0 absolute" />
-              <div className="w-28 h-5 left-[42px] top-[17px] absolute text-center text-white text-sm font-white font-['Inter']">WORKOUT APP</div>
+      <div className="max-w-full h-20 px-28 bg-gray-700 shadow justify-center items-center inline-flex">
+        <div className="w-full h-14 relative flex-col justify-start items-start flex">
+          <div className="w-52 h-14 relative">
+            <div className="w-52 h-14 left-0 top-0 absolute" />
+            <div className="flex justify-center">
+              <Image
+                src="/header_logo.svg"
+                alt="header"
+                width={200}
+                height={200}
+              />
             </div>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default header
+export default header;
